fc5e1da75cf451ac8e946ea2b636531bee76687b
attatrol <andrew.godyaev@gmail.com>
2019-12-27 02:17:55 +0300

    Debug code deleted

--- 18_1/solution.cpp
+++ 18_1/solution.cpp
@@ -5,29 +5,24 @@
  * 1. С помощью алгоритма Грэхема.
  */
 
-#define DEBUG
-
 #include <algorithm>
 #include <assert.h>
 #include <cmath>
-#ifdef DEBUG
-#include <fstream>
-#endif
 #include <iomanip>
 #include <iostream>
-#include <limits>
 #include <stack>
 #include <vector>
 
-using coord_t = long double;
+using coord_t = double;
 using point_t = std::pair<coord_t, coord_t>;
-static const double EPS = 0.;
 
-inline double calcCrossProduct(const point_t& pole, const point_t& v0, const point_t& v1)
+/* Calculate (pole; p0) x (pole; p1) */
+inline double calcCrossProduct(const point_t& pole, const point_t& p0, const point_t& p1)
 {
-    return (v0.first - pole.first) * (v1.second - pole.second) - (v1.first - pole.first) * (v0.second - pole.second);
+    return (p0.first - pole.first) * (p1.second - pole.second) - (p1.first - pole.first) * (p0.second - pole.second);
 }
 
+/* Calculate sqr|(p0, p1)| */
 inline double distanceSq(const point_t& p0, const point_t& p1)
 {
     const coord_t dX = p0.first - p1.first;
@@ -35,13 +30,10 @@ inline double distanceSq(const point_t& p0, const point_t& p1)
     return dX * dX + dY * dY;
 }
 
-template <typename T>
-int sign(const T val)
-{
-    return val < 0 ? -1 : (val > 0 ? 1 : 0);
-    //return (T(0) < val) - (val < T(0));
-}
-
+/*
+ * Find the point with min y, place it to the vector top position
+ * then sort all other points by angle value (Ox, points[0], points[i]), where Ox is the catresian horizontal axis.
+ */
 void polarSort(std::vector<point_t>& points)
 {
     std::size_t minIndex = 0;
@@ -61,7 +53,7 @@ void polarSort(std::vector<point_t>& points)
         const double crossProduct = calcCrossProduct(pole, rhs, lhs);
 
         bool result;
-        if (fabs(crossProduct) <= EPS)
+        if (fabs(crossProduct) == 0.)
         {
             result = distanceSq(pole, lhs) <= distanceSq(pole, rhs);
         }
@@ -69,23 +61,13 @@ void polarSort(std::vector<point_t>& points)
         {
             result = crossProduct < 0;
         }
-#ifdef DEBUG
-        std::cout << '(' << lhs.first << "; " << lhs.second << (result ? ") < " : ") >= (") << rhs.first << "; " << rhs.second <<")\n";
-#endif
         return result;
     };
-    std::sort(points.begin() + 1, points.end(), sorter);
 
-#ifdef DEBUG
-    std::cout << "Sorted\n";
-    for (std::size_t i = 0; i < points.size(); ++i)
-    {
-        std::cout << i << " (" << points[i].first << "; " << points[i].second << ")\n";
-    }
-    std::cout << std::endl;
-#endif
+    std::sort(points.begin() + 1, points.end(), sorter);
 }
 
+/* Find convex hull with Graham algorithm */
 std::vector<point_t> findConvexHull(const std::vector<point_t>& points)
 {
     assert(points.size() > 1);
@@ -106,47 +88,42 @@ std::vector<point_t> findConvexHull(const std::vector<point_t>& points)
         hull.push_back(nextPoint);
     }
 
-#ifdef DEBUG
-    std::cout << "Hull\n";
-    for (std::size_t i = 0; i < hull.size(); ++i)
-    {
-        std::cout << i << " = (" << hull[i].first << "; " << hull[i].second << ")\n";
-    }
-    std::cout << std::endl;
-#endif
-
     return hull;
+}
+
+/* Calculate closed polyline perimiter */
+long double calcPerimiter(const std::vector<point_t>& points)
+{
+    long double result = 0;
+    if (points.empty())
+    {
+        return result;
+    }
+    for (std::size_t i = 1; i < points.size(); ++i)
+    {
+        const coord_t dX = points[i].first - points[i - 1].first;
+        const coord_t dY = points[i].second - points[i - 1].second;
+        result += sqrt(dX * dX + dY * dY);
+    }
+    const coord_t dX = points.back().first - points.front().first;
+    const coord_t dY = points.back().second - points.front().second;
+    return result + sqrt(dX * dX + dY * dY);
 }
 
 
 int main(void)
 {
-#ifdef DEBUG
-    std::fstream cin("input.txt");
-#else
-    using std::cin;
-#endif
-
     std::size_t count;
-    cin >> count;
+    std::cin >> count;
 
     std::vector<point_t> points;
     points.reserve(count);
     for (std::size_t i = 0; i < count; ++i)
     {
         coord_t x, y;
-        cin >> x >> y;
+        std::cin >> x >> y;
         points.emplace_back(x, y);
     }
-
-#ifdef DEBUG
-    std::cout << "Input\n";
-    for (std::size_t i = 0; i < points.size(); ++i)
-    {
-        std::cout << i << " (" << points[i].first << "; " << points[i].second << ")\n";
-    }
-    std::cout << std::endl;
-#endif
 
     if (count < 2)
     {
@@ -155,22 +132,8 @@ int main(void)
     }
 
     polarSort(points);
-
     auto hull = findConvexHull(points);
-
-
-    long double perimiter = 0;
-    for (std::size_t i = 1; i < hull.size(); ++i)
-    {
-        const coord_t dX = hull[i].first - hull[i - 1].first;
-        const coord_t dY = hull[i].second - hull[i - 1].second;
-        perimiter += sqrt(dX * dX + dY * dY);
-    }
-    const coord_t dX = hull.back().first - hull.front().first;
-    const coord_t dY = hull.back().second - hull.front().second;
-    perimiter += sqrt(dX * dX + dY * dY);
-
-    std::cout << std::setprecision(9) << perimiter << '\n';
+    std::cout << std::setprecision(9) << calcPerimiter(hull) << '\n';
 
     return 0;
 }
